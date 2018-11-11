4e2f1cbc5e906d2e5a25992961f6b11b902d0ecc
Judith Smith <sjudith024@gmail.com>
2018-11-11 03:37:06 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,16 @@
-puts "Hello World!"
+def add (arg1, arg2)
+ puts arg1 + arg2 
+end 
+
+def multiply (arg1 + arg2)
+  puts arg1 * arg2
+end
+
+
+# Add 1+1 and 2 as the result
+# arg1 = 1 
+# arg2 = 1 
+add (arg1 + arg2)
+add (1, 1)
+
+ 
